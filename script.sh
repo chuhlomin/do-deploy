6f865c022e96101aa63ce4708f1d1229c95f069e
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 00:34:47 -0500

    Fixed private key usage

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ done
 echo "$SSH_KEY" > /key
 chmod 600 /key
 
-ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key.pem "docker pull ${DOCKER_IMAGE}; \
+ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${DOCKER_IMAGE}; \
     docker stop ${PLUGIN_CONTAINER_NAME}; \
     docker run --rm -d --name ${PLUGIN_CONTAINER_NAME} \
         $ENVS \
