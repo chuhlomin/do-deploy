c7e9d02c6ad9ccbe84fe3f42a2d61ae956af51dc
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 00:36:13 -0500

    Fixed docker pull

--- script.sh
+++ script.sh
@@ -17,8 +17,8 @@ done
 echo "$SSH_KEY" > /key
 chmod 600 /key
 
-ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${DOCKER_IMAGE}; \
-    docker stop ${PLUGIN_CONTAINER_NAME}; \
+ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${PLUGIN_DOCKER_IMAGE} && \
+    docker stop ${PLUGIN_CONTAINER_NAME} && \
     docker run --rm -d --name ${PLUGIN_CONTAINER_NAME} \
         $ENVS \
         $VOLUMES \
