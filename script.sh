6fe0a6f10d091aad5d753f4184850f2c6e28aac9
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 09:55:06 -0500

    Added secrets parsing

--- script.sh
+++ script.sh
@@ -8,6 +8,15 @@ do
     ENVS="${ENVS} --env ${env}"
 done
 
+SECRET_ENVS=""
+for env in $(printenv | grep "SECRET_")
+do
+    if [[ $env == SECRET_* ]]
+    then
+        SECRET_ENVS="${SECRET_ENVS} --env ${env:7}" # len("SECRET_") = 7
+    fi
+done
+
 VOLUMES=""
 for mount in $(echo $PLUGIN_MOUNTS | tr "," "\n")
 do
@@ -20,7 +29,7 @@ chmod 600 /key
 ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${PLUGIN_DOCKER_IMAGE} && \
     docker stop ${PLUGIN_CONTAINER_NAME} && \
     docker run --rm -d --name ${PLUGIN_CONTAINER_NAME} \
-        $ENVS \
+        $ENVS $SECRET_ENVS \
         $VOLUMES \
         --expose ${PLUGIN_EXPOSE} \
         --network ${PLUGIN_DOCKER_NETWORK} \
