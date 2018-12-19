91548564be1cc2e59b2aac9eaf81645863f9bc6c
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 00:10:24 -0500

    Added full script.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM alpine:3.8
 
+RUN apk --update add jq && \
+    rm -rf /var/cache/apk/*
+
 ADD script.sh /bin/
 RUN chmod +x /bin/script.sh
 
--- script.sh
+++ script.sh
@@ -1,3 +1,29 @@
 #/bin/sh
 
-printenv
+ENVS=""
+for env in $(echo $PLUGIN_ENVS | jq '. | to_entries[] | "\(.key)=\"\(.value)\""')
+do
+    ENVS="${ENVS} --env ${env}"
+done
+
+VOLUMES=""
+for mount in $(echo $PLUGIN_MOUNTS | tr "," "\n")
+do
+    VOLUMES="${VOLUMES} --volume ${mount}"
+done
+
+echo ${SSH_KEY} > /key.pem
+
+ssh ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key.pem
+
+docker pull ${DOCKER_IMAGE}
+docker stop ${PLUGIN_CONTAINER_NAME}
+docker run --rm -d --name ${PLUGIN_CONTAINER_NAME} \
+    $ENVS \
+    $VOLUMES \
+    --expose ${PLUGIN_EXPOSE} \
+    --network ${PLUGIN_DOCKER_NETWORK} \
+    --network-alias=${PLUGIN_DOCKER_NETWORK_ALIAS} \
+    ${PLUGIN_DOCKER_IMAGE}
+
+exit
