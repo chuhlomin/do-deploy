e54320768e4c30917638a6b4530dae3ffd034b38
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 19:59:41 -0500

    Debugging secrets parsing

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ for env in $(printenv | grep "SECRET_")
 do
     if [[ $env == SECRET_* ]]
     then
+        echo ${env:0:8}
         SECRET_ENVS="${SECRET_ENVS} --env ${env:7}" # len("SECRET_") = 7
     fi
 done
