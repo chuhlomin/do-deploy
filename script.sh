f4ab4259a537c1393a41c9722960b06b07f23135
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-19 19:53:04 -0500

    Debugging docker run command, 2

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 set -e
 
 ENVS=""
-for env in $(echo $PLUGIN_ENVS | jq '. | to_entries[] | "\(.key)=\"\(.value)\""')
+for env in $(echo ${PLUGIN_ENVS} | jq '. | to_entries[] | "\(.key)=\"\(.value)\""')
 do
     ENVS="${ENVS} --env ${env}"
 done
@@ -18,17 +18,18 @@ do
 done
 
 VOLUMES=""
-for mount in $(echo $PLUGIN_MOUNTS | tr "," "\n")
+for mount in $(echo ${PLUGIN_MOUNTS} | tr "," "\n")
 do
     VOLUMES="${VOLUMES} --volume ${mount}"
 done
 
-echo "$SSH_KEY" > /key
+echo "${SSH_KEY}" > /key
 chmod 600 /key
 
 ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${PLUGIN_DOCKER_IMAGE} && \
     echo \"--name ${PLUGIN_CONTAINER_NAME} \
-        $ENVS $SECRET_ENVS \
+        $ENVS \
+        $SECRET_ENVS \
         $VOLUMES \
         --expose ${PLUGIN_EXPOSE} \
         --network ${PLUGIN_DOCKER_NETWORK} \
