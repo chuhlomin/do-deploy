c1b5dee768cd5c5f9932af6af5b6c77b61bfe769
Konstantin Chukhlomin <chuhlomin@gmail.com>
2018-12-22 17:53:07 -0500

    Ignoring docker stop and wait response code

--- script.sh
+++ script.sh
@@ -30,8 +30,8 @@ echo "${SSH_KEY}" > /key
 chmod 600 /key
 
 ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${PLUGIN_DOCKER_IMAGE} && \
-    docker stop ${PLUGIN_CONTAINER_NAME} && \
-    docker wait ${PLUGIN_CONTAINER_NAME} && \
+    docker stop ${PLUGIN_CONTAINER_NAME} || true && \
+    docker wait ${PLUGIN_CONTAINER_NAME} || true && \
     docker run --rm --detach --name ${PLUGIN_CONTAINER_NAME} \
         $EXPOSE $ENVS $SECRET_ENVS $VOLUMES \
         --network ${PLUGIN_DOCKER_NETWORK} \
