fbebb032b7eea70c16ead25a6f3652aee1a04f6e
Konstantin Chukhlomin <mail@chuhlomin.com>
2019-07-23 22:33:16 -0700

    Added log-opt support

--- script.sh
+++ script.sh
@@ -24,19 +24,34 @@ do
     VOLUMES="${VOLUMES} --volume ${mount}"
 done
 
+LOG_OPT=""
+for opt in $(echo ${PLUGIN_LOG_OPT} | jq -r '. | to_entries[] | "\(.key)=\"\(.value)\""')
+do
+    LOG_OPT="${LOG_OPT} --log-opt ${opt}"
+done
+
+echo $PLUGIN_LOG_OPT
+echo $LOG_OPT
+
 if [ -z "$PLUGIN_EXPOSE" ]; then EXPOSE=""; else EXPOSE="--expose ${PLUGIN_EXPOSE}"; fi
 if [ -z "$PLUGIN_RESTART" ]; then RESTART=""; else RESTART="--restart ${RESTART}"; fi
 
 echo "${SSH_KEY}" > /key
 chmod 600 /key
 
+if [ -z $PLUGIN_CONTAINER_NAME ] && [ ! -z $PLUGIN_DOCKER_NETWORK ];
+then
+    PLUGIN_CONTAINER_NAME=$PLUGIN_DOCKER_NETWORK_ALIAS
+fi
+
 ssh -o "StrictHostKeyChecking=no" ${PLUGIN_USERNAME}@${PLUGIN_SERVER} -i /key "docker pull ${PLUGIN_DOCKER_IMAGE} && \
-    docker stop ${PLUGIN_CONTAINER_NAME} || true && \
-    docker wait ${PLUGIN_CONTAINER_NAME} || true && \
-    docker rm ${PLUGIN_CONTAINER_NAME} || true && \
-    docker run --rm --detach --name ${PLUGIN_CONTAINER_NAME} \
+    docker stop $PLUGIN_CONTAINER_NAME || true && \
+    docker wait $PLUGIN_CONTAINER_NAME || true && \
+    docker rm $PLUGIN_CONTAINER_NAME || true && \
+    docker run --rm --detach --name $PLUGIN_CONTAINER_NAME \
         $RESTART $EXPOSE $ENVS $SECRET_ENVS $VOLUMES \
-        --network ${PLUGIN_DOCKER_NETWORK} \
-        --network-alias ${PLUGIN_DOCKER_NETWORK_ALIAS} \
-        --log-driver ${PLUGIN_LOG_DRIVER} \
-        ${PLUGIN_DOCKER_IMAGE}"
+        --network $PLUGIN_DOCKER_NETWORK \
+        --network-alias $PLUGIN_DOCKER_NETWORK_ALIAS \
+        --log-driver $PLUGIN_LOG_DRIVER \
+        $LOG_OPT \
+        $PLUGIN_DOCKER_IMAGE"
